cd6ff9cce1c451bcb9ed233a7f8ee4ba204fde1f
miguelalejandrogomez <114432354+miguelalejandrogomez@users.noreply.github.com>
2022-10-17 22:28:34 -0300

    Add files via upload

---
+++ proyecto de generacion T/typer.js
@@ -0,0 +1,22 @@
+const words = [
+    'californication',
+    'plataforma5',
+    'black',
+    'summer',
+    'flea',
+    'aeroplane',
+    'peppers',
+    'unlimited',
+    'arcadium',
+    'love',
+    'getaway',
+    'stadium',
+    'quixoticelixer',
+    'quarter',
+    'snow',
+    'dylan',
+    'zephyr',
+    'funky',
+    'chili'
+  ];
+  
